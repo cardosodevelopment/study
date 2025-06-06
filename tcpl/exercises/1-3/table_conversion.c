b94f61c1a63f4f0046bc10b9de3bfd8aa96235d1
Vinicius <cardosovicius@gmail.com>
2025-06-06 12:41:45 -0300

    Some little adds.:

--- tcpl/exercises/1-3/table_conversion.c
+++ tcpl/exercises/1-3/table_conversion.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
+/* Exercise 1-3. Modify the temperature conversion program
+	to print a heading above the table. */
+
 main()
 {
-  float fahr, celsius;
-  int lower, upper, step;
+	float fahr, celsius;
+	int lower, upper, step
 
-  lower = 0;
-  upper = 300;
-  step = 20;
+	lower = 0;
+	upper = 300;
+	step = 20;
 
-  printf("Table of conversion Fahrenheit to Celsius\n");
-  printf("-----------------------------------------\n");
-  printf("Fahrenheint\tCelsius\n");
+	printf("Table of conversion Fahrenheit to Celsius\n");
+	printf("-----------------------------------------\n");
+	printf("Fahrenheint\tCelsius\n");
 
-  fahr = lower;
-  while (fahr <= upper) {
-   celsius = (5.0 / 9.0) * (fahr - 32.0);
-   printf("%3.0f\t\t%6.1f\n", fahr, celsius);
-   fahr = fahr + step;
-  }
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
+		printf("%3.0f\t\t%6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
 }
