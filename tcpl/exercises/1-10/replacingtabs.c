9c2513151fe50e223b4356e4ab72dc7ac8be326c
Vinicius <cardosovicius@gmail.com>
2025-06-16 14:26:33 -0300

    Adding the exercise 1-10.

---
+++ tcpl/exercises/1-10/replacingtabs.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/* Exercise 1-10. Write a program to copy its input to its input, replacing each
+	tab by \t, each backspace by \b, and each backslash by \\. This makes tabs and
+	backspaces visible in an unambiguous way. */
+
+main() {
+	
+	int c;
+	
+	while ((c = getchar()) != EOF) {
+		if (c == '\t') {
+			putchar(92);
+			putchar(116);
+			continue;
+		}
+		if (c == '\b') {
+			putchar(92);
+			putchar(98);
+			continue;
+		}
+		if (c == '\\') {
+			putchar(92);
+			putchar(92);
+			continue;
+		}
+
+		putchar(c);
+	}
+}
