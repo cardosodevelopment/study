f7c73d0083033bb95c1c5c7130e875a2adf50621
Vinicius <cardosovicius@gmail.com>
2025-06-23 11:13:15 -0300

    Some little changes.

--- tcpl/exercises/1-13/wordhistogram.c
+++ tcpl/exercises/1-13/wordhistogram.c
@@ -11,11 +11,10 @@ main() {
 
 	while ((c = getchar()) != EOF) {
 		if (c != ' ' && c != '\t' && c != '\n'
-			&& c != '\b') {
+			&& c != '\b') 
 			++lengthword[a];
-		} else {
+		else 
 			++a;
-		}
 	}
 
 	for (int i = 15; i > 0; --i) {
@@ -29,9 +28,7 @@ main() {
 	}
 	
 	printf("\n");
-	for (int i = 0; i < 10; ++i) {
+	for (int i = 0; i < 10; ++i)
 		printf(" %d", lengthword[i]);
-	}
-
 	printf("\n");
 }
