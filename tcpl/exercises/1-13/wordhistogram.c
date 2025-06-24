98285e331da925962c4d37299f1fe2ea4ba7f4f9
Vinicius <cardosovicius@gmail.com>
2025-06-24 12:39:59 -0300

    Added the exercise enunciation and symbolic constants.

--- tcpl/exercises/1-13/wordhistogram.c
+++ tcpl/exercises/1-13/wordhistogram.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+/*
+	Exercise 1-13. Write a program to print a histogram of the lengths of words in its input.
+	It is easy to draw the histogram with de bars horizontal; a vertical orientation is more
+	challenging.
+*/
+
+#define MAXCHAR 15
+#define MINCHAR 0
+#define FIRSTWORD 0
+#define LASTWORD 10
+
 main() {
 
 	int c;
@@ -17,9 +28,9 @@ main() {
 			++a;
 	}
 
-	for (int i = 15; i > 0; --i) {
+	for (int i = MAXCHAR; i > MINCHAR; --i) {
 		printf("\n");
-		for (int u = 0; u < 10; ++u) {
+		for (int u = FIRSTWORD; u < LASTWORD; ++u) {
 			if (lengthword[u] >= i)
 				printf(" *");
 			else
