061717fa2c1810098b12abdb81735c8d4f17238e
Vinicius <cardosovicius@gmail.com>
2025-07-02 20:33:03 -0300

    Some improvements in the code.

--- tcpl/exercises/1-17/maxchar_80.c
+++ tcpl/exercises/1-17/maxchar_80.c
@@ -1,61 +1,54 @@
 #include <stdio.h>
 
+/*
+	Exercise 1-17. Write a program to print all input lines that are longer than 80
+	characters.
+*/
+
 #define MAXLINE 1000
 #define MINIMUM 80
 
-void storage(char to[], char from[], int num_input, int length);
+void storage(char a[], char b[], int max);
 int get_line(char s[], int lim);
 
 main()
 {
-  int num_input = 0;
-  int max = 0;
-  int len;
-  char input[MAXLINE];
-  char output[MAXLINE];
+	int max = 0;
+	int len;
+	char input[MAXLINE];
+	char output[MAXLINE];
 
-  while ((len = get_line(input, MAXLINE)) > 0) {
-    if (len >= MINIMUM) {
-      ++num_input;
-      storage(output, input, num_input, max);
-    }
-    max = max + len;
-  }
-
-  printf("\n");
-  printf("%s", output);
-  printf("\n");
-  printf("Number of inputs = %d\nCharacter in the all inputs = %d\n", num_input, max);
-  
-  return 0;
+	while ((len = get_line(input, MAXLINE)) > 0) {
+		if (len >= MINIMUM) {
+			storage(output, input, max);
+			max = max + len;
+		}
+	}
+	
+	if (max > 0)
+		printf("\n%s\n", output);
+    
+	return 0;
 }
 
 int get_line(char s[], int lim)
 {
-  int i, c;
+	int i, c;
 
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-    s[i] = c;
-  }
-  if (c == '\n') {
-    s[i] = c;
-    ++i;
-  }
-  s[i] = '\0';
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
   
-  return i;
+	return i;
 }
 
-void storage(char to[], char from[], int num_input, int max)
+void storage(char to[], char from[], int max)
 {
-  int i;  
-
-  if (num_input == 1) {
-    for (i = 0; (to[i] = from[i]) != '\0'; ++i)
-      ;
-  }
-  else {
-    for (i = 0; (to[max] = from[i]) != '\0'; ++i)
-      max++;
-  }
+	int a = 0;
+	for (int i = max; (to[i] = from[a]) != '\0'; ++i)
+		++a;
 }
